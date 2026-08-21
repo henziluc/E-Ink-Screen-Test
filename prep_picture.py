a24d42b6bb2726118f87e8ad0c936673554fd670
lhenz <henzi.luca@gmail.com>
2026-08-21 23:24:19 +0200

    Try to make colors stronger

--- prep_picture.py
+++ prep_picture.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 image = Image.open("picdir/lighthouse2.jpeg")
 
@@ -7,5 +7,14 @@ image = image.resize((1200, 1600))
 
 # Make sure it's RGB
 image = image.convert("RGB")
+
+# Increase color saturation
+image = ImageEnhance.Color(image).enhance(1.8)
+
+# Increase contrast
+image = ImageEnhance.Contrast(image).enhance(1.3)
+
+# Slightly increase brightness
+image = ImageEnhance.Brightness(image).enhance(1.05)
 
 image.save("picdir/prepared.png")
