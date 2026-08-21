3045997f707d5287a13b968400455d5566c38f0c
lhenz <henzi.luca@gmail.com>
2026-08-21 23:06:20 +0200

    Changed photo file name

--- example.py
+++ example.py
@@ -56,7 +56,7 @@ try:
 
     # read bmp file 
     print("2.read bmp file")
-    Himage = Image.open(os.path.join(picdir, '13in3E.bmp'))
+    Himage = Image.open(os.path.join(picdir, 'lighthouse2.jpeg'))
     epd.display(epd.getbuffer(Himage))
     time.sleep(3)
 
