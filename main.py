869cc992c672d096b97f1f536f1f4555125d2114
lhenz <henzi.luca@gmail.com>
2026-08-22 10:47:45 +0200

    Added colored headsigns

--- main.py
+++ main.py
@@ -22,11 +22,19 @@ import json
 def display_schedule(draw, df, y_start, font, fill):
     y = y_start
     for _, row in df.iterrows():
-        
-        draw.text((50, y),row['departure_time'][:-3],font=font_small,fill=fill)
+        if row[trip_headsign] == "3":
+            fill_headsign = "green"
+        elif row[trip_headsign] == "S11":
+            fill_headsign = "black"
+        elif row[trip_headsign] == "S26":
+            fill_headsign = "black"
+        else:
+            fill_headsign = "black"
+        draw.text((50, y),row['trip_headsign'],font=font_small,fill=fill_headsign) 
         draw.text((100, y),row['route_short_name'],font=font_small,fill=fill)
-        draw.text((150, y),row['trip_headsign'],font=font_small,fill=fill) 
-        draw.text((200, y),f"+{row['delay']/60:.0f}min",font=font_small,fill=fill)  
+        draw.text((150, y),f"+{row['delay']/60:.0f}min",font=font_small,fill=fill) 
+        draw.text((200, y),row['departure_time'][:-3],font=font_small,fill=fill)
+      
         y += 20
     return draw
 
