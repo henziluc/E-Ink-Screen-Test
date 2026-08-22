7b5309941debf9d388c696ada5f2c5bf86d6bbb8
lhenz <henzi.luca@gmail.com>
2026-08-22 17:01:50 +0200

    Move weather dashboar closer to the public transport schedule

--- layout/dashboard.py
+++ layout/dashboard.py
@@ -53,7 +53,7 @@ def make_dashbord(weather_hourly, weather_daily, departures_seen, departures_etz
         draw = display_schedule(draw, "Seen", departures_seen, 50, 120, font_small, "black")
         draw = display_schedule(draw, "Etzberg", departures_etzberg, 50 , 270, font_small, "black")
         
-        draw = display_weather(draw, weather_hourly, weather_daily, 600, 120, font_small, "black")
+        draw = display_weather(draw, weather_hourly, weather_daily, 400, 120, font_small, "black")
         
         draw = draw_grid(draw, 50, 1600, 1200)
         
