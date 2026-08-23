5de14d3d9c3d0fb79fb4318aa70d7579e5c2c268
lhenz <henzi.luca@gmail.com>
2026-08-23 14:07:39 +0200

    Set up goliday widget

--- layout/dashboard.py
+++ layout/dashboard.py
@@ -24,6 +24,7 @@ import json
 from .transport_widget import display_schedule
 from .weather_widget import display_weather
 from .helpers import draw_grid
+from .holiday_widget import display_holiday
 
 font_large = ImageFont.truetype(
     "fonts/RobotoCondensed-Bold.ttf",
@@ -52,6 +53,8 @@ def make_dashbord(weather_hourly, weather_daily, departures_seen, departures_etz
         draw = display_schedule(draw, "Etzberg", departures_etzberg, 50 , 270, font_small, "black")
         
         draw = display_weather(draw, weather_hourly, weather_daily, 400, 120, font_small, "black")
+        
+        draw = display_holiday(draw)
         
         draw = draw_grid(draw, 50, 1600, 1200)
         
---
+++ layout/holiday_widget.py
@@ -0,0 +1,5 @@
+
+
+def display_holiday(draw):
+    
+    return draw
