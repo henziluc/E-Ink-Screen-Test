482a40b4453db155c378ed4021e0055362c189e7
lhenz <henzi.luca@gmail.com>
2026-08-23 19:26:08 +0200

    Test print of the holiday dates

--- layout/holiday_widget.py
+++ layout/holiday_widget.py
@@ -1,5 +1,10 @@
+import datetime
 
-
-def display_holiday(draw, df, x_start, y_start, font, fill):
-    
+def display_holiday(draw, df, x_start, y_start, font_large, font_small, fill):
+    y = y_start
+    now = datetime.datetime.now()
+    draw.text(x_start, y_start, 'Next Holiday', font=font_large, fill=fill)
+    draw.text(x_start, y_start + 70, 'Location', font=font_small, fill=fill)
+    draw.text(x_start + 50, y_start + 70, '15 days to go', font=font_small, fill=fill)
+        
     return draw
