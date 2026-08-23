85cc557288ca2d3da0d1accc64dc7a87a98c13cb
lhenz <henzi.luca@gmail.com>
2026-08-23 22:34:29 +0200

    added printing of actual location and delta T

--- layout/holiday_widget.py
+++ layout/holiday_widget.py
@@ -4,7 +4,11 @@ def display_holiday(draw, df, x_start, y_start, font_large, font_small, fill):
     y = y_start
     now = datetime.datetime.now()
     draw.text((x_start, y_start), 'Next Holiday', font=font_large, fill=fill)
-    draw.text((x_start, y_start + 70), 'Location', font=font_small, fill=fill)
-    draw.text((x_start + 50, y_start + 70), '15 days to go', font=font_small, fill=fill)
-        
+    y += 70
+    for _, row in df.iterrows():
+        delta = row['start_date'] - now
+        if delta >= 0:
+            draw.text((x_start, y_start), row['location'], font=font_small, fill=fill)
+            draw.text((x_start + 50, y_start ), str(delta) ' days to go', font=font_small, fill=fill)
+            y += 25
     return draw
