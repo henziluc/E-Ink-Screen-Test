16fe35f7597e9d34fa62f2a891be2962f67f47bc
lhenz <henzi.luca@gmail.com>
2026-08-23 22:39:32 +0200

    Fixed calculation of delta days

--- layout/holiday_widget.py
+++ layout/holiday_widget.py
@@ -2,13 +2,21 @@ import datetime
 
 def display_holiday(draw, df, x_start, y_start, font_large, font_small, fill):
     y = y_start
+    
     now = datetime.datetime.now()
+    
     draw.text((x_start, y_start), 'Next Holiday', font=font_large, fill=fill)
+    
     y += 70
+    
     for _, row in df.iterrows():
-        delta = int(row['start_date'] - now)
-        if delta >= 0:
+        
+        delta = row['start_date'] - now
+        
+        if delta >= datetime.timedelta(0):
+            days = delta.days
             draw.text((x_start, y_start), row['location'], font=font_small, fill=fill)
-            draw.text((x_start + 50, y_start ), str(delta) + ' days to go', font=font_small, fill=fill)
+            draw.text((x_start + 100, y_start ), f"{days} days to go", font=font_small, fill=fill)
             y += 25
+            
     return draw
