2e65e4f4ac6ab5c3a966924bf8e26ef08105a40d
lhenz <henzi.luca@gmail.com>
2026-08-28 16:53:37 +0200

    changed icon path

--- layout/weather_widget.py
+++ layout/weather_widget.py
@@ -120,7 +120,7 @@ def draw_weather_curve(draw, image, df_hourly, x_start, y_start, graph_height, h
 
         positions.append((positions_x, positions_y))
 
-        icon_path = BASE_DIR / "assets" / "weather_symbols" / "rain-snow-showers-night.png"
+        icon_path = BASE_DIR / "assets" / "weather_symbol" / "rain-snow-showers-night.png"
         icon = Image.open(icon_path).convert("RGB")
         icon = icon.resize((40, 40))
 
