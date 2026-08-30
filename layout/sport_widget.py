01c437bf6e9722759bb12f1b6d6da0daf6d409fb
lhenz <henzi.luca@gmail.com>
2026-08-30 22:13:24 +0200

    added font size and fill to text

--- layout/sport_widget.py
+++ layout/sport_widget.py
@@ -12,12 +12,12 @@ def display_health_widget(draw, image, x_start, y_start, health_data):
     step_icon_path = BASE_DIR / "assets" / "sport_symbol" / "shoe-prints.png"
     heart_icon_path = BASE_DIR / "assets" / "sport_symbol" / "heart.png"
     
-    draw.text((x_start, y), 'Health Stats', font=font_large, fill=fill_main)
+    draw.text((x_start, y), 'Health Stats', font = font_large, fill = fill_main)
     
     y += spacing_large
     
     
-    # draw steps    
+    # draw actual steps / target steps    
     step_icon = Image.open(step_icon_path).convert("RGBA")
     step_icon = step_icon.resize((icon_size, icon_size))
     
@@ -25,5 +25,7 @@ def display_health_widget(draw, image, x_start, y_start, health_data):
     target_steps = str(health_data['step_goal'])
             
     image.paste(step_icon, (x_start, y), step_icon)
-    draw.text((x_start + icon_size, y), actual_steps + ' / ' + target_steps )
+    draw.text((x_start + icon_size, y), actual_steps + ' / ' + target_steps, font = font_small, fill = fill_main )
+    
+    
      
