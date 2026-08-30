d83c27f28bce2ba9d8ca5ad184a0f4197addcec6
lhenz <henzi.luca@gmail.com>
2026-08-30 22:30:11 +0200

    added body battery and sleep score

--- layout/sport_widget.py
+++ layout/sport_widget.py
@@ -9,12 +9,14 @@ def display_health_widget(draw, image, x_start, y_start, health_data):
     y = y_start
     icon_size = 25
     
+    # define all icon paths
     step_icon_path = BASE_DIR / "assets" / "sport_symbol" / "shoe-prints.png"
     battery_full_icon_path = BASE_DIR / "assets" / "sport_symbol" / "battery-full.png"
     battery_three_quarters_icon_path = BASE_DIR / "assets" / "sport_symbol" / "battery-three-quarters.png"
     battery_half_icon_path = BASE_DIR / "assets" / "sport_symbol" / "battery-half.png"
     battery_quarter_icon_path = BASE_DIR / "assets" / "sport_symbol" / "battery-quarter.png"
     battery_empty_icon_path = BASE_DIR / "assets" / "sport_symbol" / "battery-empty.png"   
+    sleep_icon_path = BASE_DIR / "assets" / "sport_symbol" / "bed.png"
     heart_icon_path = BASE_DIR / "assets" / "sport_symbol" / "heart.png"
     
     draw.text((x_start, y), 'Health Stats', font = font_large, fill = fill_main)
@@ -32,30 +34,36 @@ def display_health_widget(draw, image, x_start, y_start, health_data):
     draw.text((x_start + icon_size + 5, y), actual_steps + ' / ' + target_steps, font = font_small, fill = fill_main )
     
     y += spacing_small
+    
     # draw body battery
-    step_battery = Image.open(battery_full_icon_path).convert("RGBA")
-    step_battery = step_battery.resize((icon_size, icon_size))
-    image.paste(step_battery, (x_start, y), step_battery)    
+    body_battery = health_data['body_battery']
+    
+        #choose which battery icon depening on body battery
+    if body_battery >= 90:
+        battery_icon = Image.open(battery_full_icon_path).convert("RGBA")
+    elif body_battery >= 60:
+        battery_icon = Image.open(battery_three_quarters_icon_path).convert("RGBA")
+    elif body_battery >= 40:
+        battery_icon = Image.open(battery_half_icon_path).convert("RGBA")
+    elif body_battery >= 10:
+        battery_icon = Image.open(battery_quarter_icon_path).convert("RGBA")
+    else:       
+        battery_icon = Image.open(battery_empty_icon_path).convert("RGBA")
+    
+    battery_icon = battery_icon.resize((icon_size, icon_size))
+    image.paste(battery_icon, (x_start, y), battery_icon)    
+    
+    draw.text((x_start + icon_size + 5, y), str(body_battery), font = font_small, fill = fill_main )
+    
     y += spacing_small
     
-    step_battery = Image.open(battery_three_quarters_icon_path).convert("RGBA")
-    step_battery = step_battery.resize((icon_size, icon_size))
-    image.paste(step_battery, (x_start, y), step_battery)    
-    y += spacing_small
-        
-    step_battery = Image.open(battery_half_icon_path).convert("RGBA")
-    step_battery = step_battery.resize((icon_size, icon_size))
-    image.paste(step_battery, (x_start, y), step_battery)    
-    y += spacing_small
-            
-    step_battery = Image.open(battery_quarter_icon_path).convert("RGBA")
-    step_battery = step_battery.resize((icon_size, icon_size))
-    image.paste(step_battery, (x_start, y), step_battery)    
-    y += spacing_small
-                
-    step_battery = Image.open(battery_empty_icon_path).convert("RGBA")
-    step_battery = step_battery.resize((icon_size, icon_size))
-    image.paste(step_battery, (x_start, y), step_battery)    
-    y += spacing_small
     # draw sleep
-     
+    sleep_icon = Image.open(sleep_icon_path).convert("RGBA")
+    
+    sleep_icon = sleep_icon.resize((icon_size, icon_size))
+    image.paste(sleep_icon, (x_start, y), sleep_icon)
+    
+    sleep_hours = str(health_data['sleep_hours'])
+    sleep_score = str(health_data['sleep_score'])
+    
+    draw.text((x_start + icon_size + 5, y), sleep_hours + 'h, ' + sleep_score + 'P' , font = font_small, fill = fill_main )    
