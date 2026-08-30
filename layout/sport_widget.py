c14f72de2fa2bafbdf9963f68106e941ad7829a3
lhenz <henzi.luca@gmail.com>
2026-08-30 19:47:06 +0200

    start implementing sport widget

--- layout/sport_widget.py
+++ layout/sport_widget.py
@@ -1,11 +1,21 @@
+from PIL import Image
+from pathlib import Path
+
 from .fonts import font_small, font_normal, font_medium, font_large, fill_main, spacing_small, spacing_normal, spacing_medium, spacing_large
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-def display_health_widget(draw, x_start, y_start):
+def display_health_widget(draw, image, x_start, y_start):
     y = y_start
     
     draw.text((x_start, y), 'Health Stats', font=font_large, fill=fill_main)
     
     y += spacing_large
+    
+    icon_path = BASE_DIR / "assets" / "sport_symbol" / "shoe-print.png"
+        
+    icon = Image.open(icon_path).convert("RGBA")
+    icon = icon.resize((30, 30))
+            
+    image.paste(icon, (x_start, y), icon)
      
