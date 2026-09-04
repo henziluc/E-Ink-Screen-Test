d95bea2927b627b7b6b1fb145d80c81f6193f01f
lhenz <henzi.luca@gmail.com>
2026-09-04 17:29:02 +0200

    Commented function

--- layout/welcome_widget.py
+++ layout/welcome_widget.py
@@ -12,14 +12,12 @@ moon_half_icon_path = BASE_DIR / "assets" / "moon_symbol" / "moon-phases_3.png"
 moon_1_4_icon_path = BASE_DIR / "assets" / "moon_symbol" / "moon-phases_4.png"
 
 def display_welcome(draw, image, x_start, y_start, moon_data):
-    
-    
     x_end = 1200 - 2 * x_start
     y_end = font_massiv.size + y_start
     now = datetime.datetime.now()   
-    #now_hour = now.hour
-    now_hour = 22
+    now_hour = now.hour
     
+    # Determine the appropriate greeting based on the current hour
     if 4 <= now_hour < 12:
         text = 'Good Morning'
     elif 12 <= now_hour < 18:
@@ -28,7 +26,9 @@ def display_welcome(draw, image, x_start, y_start, moon_data):
         text = 'Good Evening'
     else:
         text = 'Good Night'
-        display_moon_phase(image, 800, y_start + 20, moon_data)
+        # Display moon phase icon if it's night time
+        display_moon_phase(image, 790, y_start + 20, moon_data)
+        
     draw_centered_text(draw, text,(x_start, y_start, x_end, y_end), font_massiv, fill_main)
     
     
@@ -36,6 +36,7 @@ def display_moon_phase(image, x_start, y_start, moon_data):
     icon_size = 60
     illumination = moon_data['illumination']
     
+    # Choose path of moon icon based on illumination
     if illumination > 87:
         moon_icon_path = moon_full_icon_path
     elif illumination > 62:
@@ -45,9 +46,13 @@ def display_moon_phase(image, x_start, y_start, moon_data):
     else:
         moon_icon_path = moon_1_4_icon_path
     
+    # Display moon icon if illumination is greater than 2%
     if illumination > 2:
         moon_icon = Image.open(moon_icon_path).convert("RGBA")    
         moon_icon = moon_icon.resize((icon_size, icon_size))
-        if moon_data['waxing']:
+        
+        # Mirror the moon icon if it's waxing
+        if not moon_data['waxing']:
             moon_icon = ImageOps.mirror(moon_icon)
+            
         image.paste(moon_icon, (x_start, y_start), moon_icon)
