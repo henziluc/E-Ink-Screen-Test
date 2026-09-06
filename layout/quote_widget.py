c3f98d86470883542205a548a8efc1f52cf40bb1
lhenz <henzi.luca@gmail.com>
2026-09-06 23:03:13 +0200

    change that only one random quote with wrap text is shown

--- layout/quote_widget.py
+++ layout/quote_widget.py
@@ -1,12 +1,29 @@
 
-from .fonts import font_small, font_normal, font_medium, font_large, fill_main, spacing_small, spacing_normal, spacing_medium, spacing_large
+import random
 
+from .fonts import font_small, font_normal, font_medium, font_large, fill_main, spacing_small, spacing_normal, spacing_medium, spacing_large
+from .helpers import  wrap_text_to_width
 
 def display_quote_widget(draw, x_start, y_start, quote_data):
     y = y_start
     # Draw quote data
-    for quote in quote_data:
-        draw.text((x_start, y), quote['title'], font=font_medium, fill=fill_main)
-        y += spacing_normal
-        draw.text((x_start, y), quote['description'], font=font_small, fill=fill_main)
-        y += spacing_large
+    draw.text((x_start, y), "Quote", font=font_large, fill=fill_main)
+    y += spacing_large
+    quote = random.sample(quote_data, min(len(quote_data), 1))  # Select up to 3 random quotes
+    
+    draw.text((x_start, y), quote['title'], font=font_medium, fill=fill_main)
+    y += spacing_normal
+    
+    lines = wrap_text_to_width(
+        quote['description'],
+        font_small,
+        max_width=320,
+        draw=draw,
+        max_lines=3
+    )
+    
+    for line in lines:
+        draw.text((x_start, y), line, font=font_small, fill=fill_main)
+        y += spacing_small
+                
+    
