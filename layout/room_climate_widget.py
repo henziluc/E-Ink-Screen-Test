23bec0c317df6215fa0f4df3e8d952cbcda11833
lhenz <henzi.luca@gmail.com>
2026-09-08 22:04:09 +0200

    Fixed error

--- layout/room_climate_widget.py
+++ layout/room_climate_widget.py
@@ -19,10 +19,8 @@ def display_room_climate_widget(draw, image, x_start, y_start, room_climate_data
     
     draw.line((x_start, y, x_start, y - graph_height), fill=fill_main, width=2)
     draw.line((x_start, y, x_start + graph_width, y), fill=fill_main, width=2)
-    print(type(df))
-    print(df)
-    print(df.columns)
-    draw_room_climate_graph(draw, x_start, y - graph_height, graph_width, graph_height, df, "temperature", fill_main)
+
+    draw_room_climate_graph(draw, x_start, y, graph_width, graph_height, df, "temperature", fill_main)
     
     y += spacing_small
     
@@ -32,7 +30,7 @@ def display_room_climate_widget(draw, image, x_start, y_start, room_climate_data
     
     draw.line((x_start, y, x_start, y - graph_height), fill=fill_main, width=2)
     draw.line((x_start, y, x_start + graph_width, y), fill=fill_main, width=2)
-    draw_room_climate_graph(draw, x_start, y - graph_height, graph_width, graph_height, df, "humidity", fill_main)
+    draw_room_climate_graph(draw, x_start, y, graph_width, graph_height, df, "humidity", fill_main)
     y += spacing_small
         
     # Draw CO2
@@ -41,7 +39,7 @@ def display_room_climate_widget(draw, image, x_start, y_start, room_climate_data
     
     draw.line((x_start, y, x_start, y - graph_height), fill=fill_main, width=2)
     draw.line((x_start, y, x_start + graph_width, y), fill=fill_main, width=2)
-    draw_room_climate_graph(draw, x_start, y - graph_height, graph_width, graph_height, df, "co2", fill_main)
+    draw_room_climate_graph(draw, x_start, y, graph_width, graph_height, df, "co2", fill_main)
     
     
     
